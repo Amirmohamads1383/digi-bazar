9276d87d0a59ac5e0b905f46ecbd5f2bad05638c
Amirmohamads1383 <161164840+Amirmohamads1383@users.noreply.github.com>
2026-07-14 12:35:02 +0330

    Complete XL Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,89 +1,31 @@
-import React, { useState } from "react";
+import TopHeader from "./components/TopHeader";
+import BottomHeader from "./components/BottomHeader";
 
 export default function Header() {
-  const [searchInput, setSearchInput] = useState("");
+  const menu = [
+    {
+      title: "صفحه اصلی",
+      path: "/",
+    },
+    {
+      title: " سبد های گوشی",
+      path: "/price-list",
+    },
+    {
+      title: " قیمت عمده",
+      path: "/price-list",
+    },
+    {
+      title: " درخواست پنل همکار",
+      path: "/price-list",
+    },
+  ];
   return (
     <>
-      <header className="container py-7">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-8">
-            <div className="flex items-center gap-1.5">
-              <span className="text-white w-9 h-9 bg-Primary rounded-xl flex items-center justify-center shadow-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="m20 7-8 4-8-4"
-                    stroke="currentColor"
-                    strokeWidth={1.5}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="m18.625 4.668-5-2.31a3.86 3.86 0 0 0-3.25 0l-5 2.31C3.931 5.335 3 6.823 3 8.466v7.068c0 1.643.93 3.131 2.375 3.798l5 2.31a3.86 3.86 0 0 0 3.25 0l5-2.31C20.069 18.665 21 17.177 21 15.534V8.466c0-1.643-.93-3.131-2.375-3.798M12 11v11"
-                    stroke="currentColor"
-                    strokeWidth={1.5}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </span>
-              <h1 className="text-2xl font-bold">دیجی بازار</h1>
-            </div>
-            <div className="relative w-100">
-              <input
-                type="text"
-                value={searchInput}
-                onChange={(e) => {
-                  setSearchInput(e.target.value);
-                }}
-                placeholder="جست و جو"
-                className="w-full p-2 border-2 border-Secondry outline-0 rounded-lg"
-              />
-              <button className="absolute left-2 top-1/2 -translate-y-1/2 text-Secondry cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M18.5 18.5 22 22m-1-10.5a9.5 9.5 0 1 0-19 0 9.5 9.5 0 0 0 19 0"
-                    stroke="currentColor"
-                    strokeWidth={1.5}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="flex items-center gap-1.5 ">
-            <span className="text-Primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  d="M21 19v-1.646a2 2 0 0 0-1.257-1.857l-2.034-.813a2 2 0 0 0-2.532.962L15 16s-2.5-.5-4.5-2.5S8 9 8 9l.354-.177a2 2 0 0 0 .963-2.532l-.814-2.034A2 2 0 0 0 6.646 3H5a2 2 0 0 0-2 2c0 8.837 7.163 16 16 16a2 2 0 0 0 2-2Z"
-                  stroke="currentColor"
-                  strokeWidth={1.5}
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </span>
-            <span className="text-base font-medium text-Caption">
-              021-77046390
-            </span>
-          </div>
+      <header className="py-7 bg-white shadow-lg">
+        <div className="container flex flex-col gap-6">
+          <TopHeader />
+          <BottomHeader menu={menu}/>
         </div>
       </header>
     </>
---
+++ src/components/Header/components/BottomHeader.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link } from "react-router";
+
+export default function BottomHeader({ menu }) {
+    const [inCart, setInCart] = useState(0);
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <div>
+          <h4 className="flex items-center gap-1.5 text-black font-semibold border-l border-l-Caption pl-5 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M2 4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm12 0a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2zM2 16a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm12 0a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2z"
+                stroke="currentColor"
+                strokeWidth={1.5}
+                strokeLinejoin="round"
+              />
+            </svg>
+            دسته بندی ها
+          </h4>
+        </div>
+        <div className="flex items-center gap-6">
+          {menu.map((item) => (
+            <Link key={item.path} to={item.path}>
+              <span className="font-normal text-Caption hover:text-black transition-all">
+                {item.title}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="flex items-center gap-3">
+        <button className="text-Title relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M11 20.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm9 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+              stroke="currentColor"
+              strokeWidth={1.5}
+            />
+            <path
+              d="M6 4h12a4 4 0 0 1 4 4v5a4 4 0 0 1-4 4h-8a4 4 0 0 1-4-4zm0 0a2 2 0 0 0-2-2H2m4 6h15.5"
+              stroke="currentColor"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="absolute -top-3 -right-3.5 w-5 h-5 flex items-center justify-center text-xs bg-Primary text-white rounded-full">
+            {inCart}
+          </span>
+        </button>
+        <button className="px-6 py-2.5 font-medium bg-Primary text-white hover:bg-white hover:text-Primary rounded-full cursor-pointer transition-all">
+          ورود / ثبت نام
+        </button>
+      </div>
+    </div>
+  );
+}
